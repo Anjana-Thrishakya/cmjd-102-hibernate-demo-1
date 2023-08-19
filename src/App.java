1a230ca0f3f2824ab847fd514ce8ef479308c6b5
Anjana Thrishakya <anjanathrishakya@gmail.com>
2023-08-19 12:25:24 +0530

    Student save

--- src/App.java
+++ src/App.java
@@ -1,10 +1,23 @@
 
-import org.hibernate.Session;
+import java.util.ArrayList;
+import java.util.List;
 
-import util.SessionFactoryConfiguration;
+import entity.StudentEntity;
+import entity.embeded.StudentName;
+import repository.StudentRepository;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Session s = SessionFactoryConfiguration.getInstance().getSession();
+        StudentRepository repo = new StudentRepository();
+        StudentEntity  entity = new StudentEntity();
+        entity.setNic("891676789V");
+        StudentName studentName = new StudentName("Stu 1", "stu Last 1");
+        entity.setName(studentName);
+        List<String> mobiles = new ArrayList<>();
+        mobiles.add("0775675567");
+        mobiles.add("0715675567");
+        entity.setMobiles(mobiles);
+
+        repo.saveStudent(entity);
     }
 }
